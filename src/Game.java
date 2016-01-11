5c6e18876dc29143770da59c87aad1b3bf07760c
vmandrews <vanessa.andrews42@gmail.com>
2016-01-11 21:12:50 +1100

    Renamed Exploro.java to Game.java, made attributes and methods non-static

---
+++ src/Game.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Game {
+	private ArrayList<Room> rooms;
+	
+	public static void main(String[] args) {
+		Game g = new Game();
+		g.gameInit();
+	}
+
+	/**
+	 * Initializes game world.
+	 * Draws rooms and objects.
+	 */
+	public void gameInit() {
+		
+	}
+}
